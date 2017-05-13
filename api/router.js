3d7700fab6669b2845c2fc5c5014d55617835fb7
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-05-12 22:54:25 -0700

    initial commit

---
+++ api/router.js
@@ -0,0 +1,10 @@
+const myRouter = require('express').Router();
+// const controller = require('./controller');
+// const db = require('./schema');
+
+// myRouter.route('/endpoint')
+//   .post((req, res) => {
+//     controller.someFunction(req, res);
+//   });
+
+module.exports = myRouter;
